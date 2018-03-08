92e672394a39db53bb1c28af75566effaeb72ab3
tasanoers <tamaki.asano@ersnet.org>
2018-03-08 16:10:43 +0100

    Try to create store

---
+++ src/components/Sample.vue
@@ -0,0 +1,41 @@
+<template>
+  <div class="sample">
+    <h1>{{ msg }}</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'sample',
+  /*
+  data () {
+    return {
+      msg: 'Welcome to My Vuex Sample'
+    }
+  },
+  */
+  computed: {
+    msg () {
+      return this.$store.msg
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h1, h2 {
+  font-weight: normal;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
--- src/main.js
+++ src/main.js
@@ -3,13 +3,23 @@
 import Vue from 'vue'
 import App from './App'
 import router from './router'
+import Vuex from 'vuex'
 
 Vue.config.productionTip = false
+
+Vue.use(Vuex)
+
+const store = new Vuex.Store({
+  state: {
+    msg: 'Hello Vuex Store.'
+  }
+})
 
 /* eslint-disable no-new */
 new Vue({
   el: '#app',
   router,
   components: { App },
-  template: '<App/>'
+  template: '<App/>',
+  store
 })
